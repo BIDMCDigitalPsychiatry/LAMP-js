abff5098fa53df64dbd93786e2a7adebbd058879
Facundo Almeida <falmeida@orangeloops.com>
2022-04-27 17:43:44 -0300

    Remove password requirement when creating OAuth credentials

--- src/service/OAuth.service.ts
+++ src/service/OAuth.service.ts
@@ -1,4 +1,10 @@
-import { Fetch, Configuration } from "./Fetch";
+import { Fetch, Configuration } from './Fetch';
+
+export interface OAuthParams {
+  serverAddress?: string
+  codeVerifier?: string
+  codeChallenge?: string
+}
 
 export interface AuthResponse {
   success: boolean | undefined,
@@ -6,23 +12,63 @@ export interface AuthResponse {
   refresh_token: string | undefined,
 }
 export class OAuthService {
-    public configuration?: Configuration
+  public configuration?: Configuration
+  
+  public get is_enabled(): boolean {
+    return JSON.parse(sessionStorage?.getItem("LAMP._oauth_enabled")) as boolean ?? false
+  }
 
-    public async startOauthFlow() : Promise<string>{
-        return await Fetch.get<string>("/oauth/start", this.configuration);
+  public set is_enabled(value: boolean) {
+    sessionStorage?.setItem("LAMP._oauth_enabled", JSON.stringify(value))
+  }
+
+  public get params(): OAuthParams {
+    try {
+      return JSON.parse(sessionStorage?.getItem("LAMP._oauth"))
+    } catch {
+      return { serverAddress: null, codeVerifier: null }
+    }
+  }
+
+  public set params(value: OAuthParams) {
+    this.configuration = {
+      ...this.configuration,
+      base: value.serverAddress ?? "api.lamp.digital"
+    }
+    sessionStorage?.setItem("LAMP._auth", JSON.stringify({
+      serverAddress: value.serverAddress
+    }))
+
+    // Save the authorization in sessionStorage for later.
+    sessionStorage?.setItem("LAMP._oauth", JSON.stringify(value))
+  }
+
+  public async start_flow(): Promise<URL>{
+    let url: URL
+
+    const urlString = (await Fetch.get<{url: string}>("/oauth/start", this.configuration)).url
+    if (!urlString) {
+      url = null
+    } else {
+      try {
+        url = new URL(urlString)
+      } catch (error) {
+        throw Error(`Invalid start URL ${urlString}`)
+      }
     }
 
-    public async requestAuthorization(
-      code: string,
-      codeVerifier: string
-    ) : Promise<AuthResponse> {
-      return await Fetch.post(
-        "/oauth/authenticate",
-        {
-          code: code,
-          code_verifier: codeVerifier
-        },
-        this.configuration
-      )
-    }
+    this.is_enabled = !!url
+    return url
+  }
+
+  public async request_authorization(code: string): Promise<AuthResponse> {
+    return await Fetch.post(
+      "/oauth/authenticate",
+      {
+        code: code,
+        code_verifier: this.params.codeVerifier
+      },
+      this.configuration
+    )
+  }
 }
