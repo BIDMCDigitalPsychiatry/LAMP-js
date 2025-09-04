be76ecec651185dbc88111f56e46a6758359f5ba
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2025-09-04 17:33:44 +0530

    Researcher -setting add new fields

--- src/model/ResearcherSettings.ts
+++ src/model/ResearcherSettings.ts
@@ -3,38 +3,54 @@ import { Identifier, Timestamp } from './Type'
 /**
  *
  */
+
+public id ?: Identifier
+  public bannerGreeting ?: string
+  public bannerHeading ?: string
+  public bannerSubHeading ?: string
+  public imageBase64 ?: string
+  public selectedActivity ?: string
+  public studyId ?: string
 export class ResearcherSettings {
-    
+
     /**
      * The banner greeting.
      */
     bannerGreeting?: string
-    
+
     /**
      * The banner heading
      */
     bannerHeading?: string
-    
+
     /**
      * The banner sub heading
      */
     bannerSubHeading?: string
-    
+
     /**
      * The image icon for banner
      */
     imageBase64?: string
-    
+
+    /**
+     * Activity selected for banner (if any)
+     */
+    selectedActivity?: string
+    /**
+     * The study ID this setting is associated with
+     */
+    studyId?: string
     /**
      * The timestamp
      */
-   timestamp?: Timestamp
-    
+    timestamp?: Timestamp
+
     /**
      * The deleted flag
      */
     deleted?: boolean
-    
+
 }
 
 export type ResearcherBanner = {
